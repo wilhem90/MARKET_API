d64e50f91cca689098970a2166c976940a6b3808
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-01-25 00:07:39 -0300

    Fresh1

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express")
 const app = express()
+
 const {MongoClient, ServerApiVersion} = require("mongodb")
 const cors = require("cors")
+
+const dotenv = require("dotenv").config()
+console.log(`A porta é: ${process.env.PORT}`);
+console.log(`A sua senha é: ${process.env.SENHA_TO_CONECT}`);
+
+console.log(app);
